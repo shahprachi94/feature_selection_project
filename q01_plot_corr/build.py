fdbd82f82698726f964c7a5c86c89fd43fdc7fb6
shahprachi94 <prachirshah1994@gmail.com>
2017-11-05 12:04:57 +0000

    Done

--- q01_plot_corr/build.py
+++ q01_plot_corr/build.py
@@ -1,8 +1,10 @@
+# %load q01_plot_corr/build.py
 # Default imports
 import pandas as pd
 from matplotlib.pyplot import yticks, xticks, subplots, set_cmap
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
-
+def plot_corr(df, size = 11):
+    return
 # Write your solution here:
